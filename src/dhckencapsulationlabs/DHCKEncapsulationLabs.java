2bc6bbd63be66a23e87c291ea97286a95433a2a0
dkerr <dkerr@L11706038602.wctc.msft>
2012-08-29 18:13:44 -0500

    Reverting to demo original

--- src/dhckencapsulationlabs/DHCKEncapsulationLabs.java
+++ src/dhckencapsulationlabs/DHCKEncapsulationLabs.java
@@ -15,6 +15,5 @@ public class DHCKEncapsulationLabs {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("goodbye");
     }
 }
